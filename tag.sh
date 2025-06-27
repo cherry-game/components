2258ae366152bc41a9a6b8a7ca06eafbb6a60804
phantacix <phantacix@github.com>
2025-06-27 21:24:15 +0800

    fix: update tag paths in tag.sh to remove redundant 'components/' prefix

--- tag.sh
+++ tag.sh
@@ -9,30 +9,30 @@ echo ""
 
 
 echo "[TAG ${number}] components/cron"
-git tag -a "components/cron/v${number}" -m "auto tag"
+git tag -a "cron/v${number}" -m "auto tag"
 
 
 echo "[TAG ${number}] components/data-config"
-git tag -a "components/data-config/v${number}" -m "auto tag"
+git tag -a "data-config/v${number}" -m "auto tag"
 
 
 echo "[TAG ${number}] components/etcd"
-git tag -a "components/etcd/v${number}" -m "auto tag"
+git tag -a "etcd/v${number}" -m "auto tag"
 
 
 echo "[TAG ${number}] components/gin"
-git tag -a "components/gin/v${number}" -m "auto tag"
+git tag -a "gin/v${number}" -m "auto tag"
 
 
 echo "[TAG ${number}] components/gops"
-git tag -a "components/gops/v${number}" -m "auto tag"
+git tag -a "gops/v${number}" -m "auto tag"
 
 
 echo "[TAG ${number}] components/gorm"
-git tag -a "components/gorm/v${number}" -m "auto tag"
+git tag -a "gorm/v${number}" -m "auto tag"
 
 
 echo "[TAG ${number}] components/mongo"
-git tag -a "components/mongo/v${number}" -m "auto tag"
+git tag -a "mongo/v${number}" -m "auto tag"
 
 echo "[TAG] ------------------------------------------------------"
